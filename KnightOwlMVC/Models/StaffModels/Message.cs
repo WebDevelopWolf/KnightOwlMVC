8851ec42697524f3afabb1aaaf36f8021cc6ca63
Liane Stevenson <knightowlcrm@gmail.com>
2016-01-23 20:23:53 +0000

    Mail
    Added Mail Class

---
+++ KnightOwlMVC/Models/StaffModels/Message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KnightOwlMVC.Models.StaffModels
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+
+        public virtual Staff ToStaff { get; set; }
+
+        public virtual Staff FromStaff { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public bool Read { get; set; }
+
+        public bool Important { get; set; }
+
+        public bool Draft { get; set; }
+
+        public bool Trash { get; set; }
+    }
+}
