7115cb1accb52aac0af38ecc0c057d206cfd529c
Liane Stevenson <knightowlcrm@gmail.com>
2016-01-23 19:16:24 +0000

    Marketing
    Marketing models loaded in

---
+++ KnightOwlMVC/Models/MarketingModels/SocialMedia.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KnightOwlMVC.Models.MarketingModels
+{
+    public class SocialMedia
+    {
+        public int SocialMediaId { get; set; }
+
+        public string Type { get; set; }
+
+        public int SocialId { get; set; }
+
+        public string SocialUnique { get; set; }
+    }
+}
