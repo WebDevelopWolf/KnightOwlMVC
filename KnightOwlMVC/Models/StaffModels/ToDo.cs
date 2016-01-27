084d00eb989627a62e8b03f7b605cbb9fe3bda75
Liane Stevenson <knightowlcrm@gmail.com>
2016-01-27 23:54:31 +0000

    Dash - Social Media
    Added Social Media to dashboard

---
+++ KnightOwlMVC/Models/StaffModels/ToDo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KnightOwlMVC.Models.StaffModels
+{
+    public class ToDo
+    {
+        public int ToDoId { get; set; }
+
+        public string Priority { get; set; }
+
+        [Required(ErrorMessage = "To-do must have a body")]
+        public string Text { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime Date { get; set; }
+
+        public bool Complete { get; set; }
+
+        public Staff StaffId { get; set; }
+    }
+}
